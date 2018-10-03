663f4a3d9015eecca9d4c66b2ed745a8629b914c
FloppyDisk <rainbowdjdash1@gmail.com>
2018-10-03 16:33:56 -0700

    Added extremefps.config.drawmonitors

--- lua/autorun/extremeconfig.lua
+++ lua/autorun/extremeconfig.lua
@@ -21,14 +21,14 @@ extremefps.config.facial.eyes = false -- Disable or enable eyes on players and n
 extremefps.config.facial.teeth = true -- Disable or enable Teeth on players and npcs.
 
 extremefps.config.sprays = true -- Disable or enable sprays
-extremefps.config.ropes = true -- Disable some core aspects of the ropes. This does not disable ropes entirely, it just disables things that can take up some GPU usage
+extremefps.config.ropes = false -- Disable some core aspects of the ropes. This does not disable ropes entirely, it just disables things that can take up some GPU usage
 extremefps.config.details = true -- Disable minor details. These can boost FPS up by 10-20.
 extremefps.config.shadows = true -- Disable Shadows. This includes props, players and entities
 extremefps.config.shadowsflashlight = false -- Disables Shadows and Flashlights. Does not have much effect on FPS. You should probably make this true so you can use the flashlight
 extremefps.config.gibs = true -- Disable Gibs (Basically Anything gore. And decals of props when you destroy them like wooden crates)
 extremefps.config.ragdolls = true -- Disable ragdolls.
 extremefps.config.rlod = false -- This can freeze your game. This will fuck up your cars if this is set to anything above 1
-
+extremefps.config.drawmonitors = true -- I suggest this is kept true because if you
 
 
 -- [[Debug]]
@@ -39,6 +39,6 @@ Turns on the FPS counter in top right
 and turns on cl_showpos (Showing your posisition in the mapo and your speed velocity.
 Turns on net_graph to 2
 
-Usfull for testing what settings you should enable in the config
+Useful for testing what settings you should enable in the config
 --]]
 extremefps.config.debug = false
