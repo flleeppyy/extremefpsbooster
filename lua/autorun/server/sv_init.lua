65514be4da9ba135e7c9c1160d26b1e93cd26662
FloppyDisk <rainbowdjdash1@gmail.com>
2018-10-14 00:22:10 -0700

    Update sv_init.lua

--- lua/autorun/server/sv_init.lua
+++ lua/autorun/server/sv_init.lua
@@ -2,7 +2,7 @@ util.AddNetworkString( "ExtremeFpsBoost" )
 
 hook.Add( "PlayerInitialSpawn", "ExtremeFpsBoostFunction", function ( ply )
      timer.Simple(2, function()
-          net.Start( "ExtremeFpsBoost" ) 
+          net.Start( "ExtremeFpsBoost" )
           net.Send( ply )
      end)
 end)
